4922cf4e90b4a36d1d294362520a2301e7b6ac05
Krishna Patel <softeng.krishna@gmail.com>
2015-04-30 15:44:23 +0530

    dfgjrtfj

--- app/src/main/java/com/example/android/parvarish_nutricalculator/ui/GuideLinesSubScreen.java
+++ app/src/main/java/com/example/android/parvarish_nutricalculator/ui/GuideLinesSubScreen.java
@@ -1,11 +1,18 @@
 package com.example.android.parvarish_nutricalculator.ui;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,6 +22,8 @@ import com.example.android.parvarish_nutricalculator.helpers.AdvancedSpannableSt
 import java.util.ArrayList;
 
 public class GuideLinesSubScreen extends ActionBarActivity {
+
+    private GridView imageList;
 
     private String colors[] = {"#0A82C8", "#EA0083", "#F7760E", "#00A045", "#CF332F"};
     private String topics[] = {"To meet Energy and Protein requirements,\nincrease intake of:",
@@ -34,7 +43,15 @@ public class GuideLinesSubScreen extends ActionBarActivity {
         selectedTopic = extra.getInt("who");
         txtGuideLineSubPageHeading = (TextView) findViewById(R.id.txtGuideLineSubPageHeading);
 
+
+        imageList = (GridView)findViewById(R.id.imageList);
+
         setupTopicHeading();
+
+        CustomImageAdapter adp = new CustomImageAdapter(GuideLinesSubScreen.this);
+        imageList.setAdapter(adp);
+
+
     }
 
     private void setupTopicHeading() {
@@ -98,6 +115,58 @@ public class GuideLinesSubScreen extends ActionBarActivity {
 
 
     }
+
+    class CustomImageAdapter extends BaseAdapter {
+        LayoutInflater layoutInflator;
+        private Context ctx;
+        public CustomImageAdapter(Context ctx){
+            this.ctx = ctx;
+        }
+
+        @Override
+        public int getCount() {
+            return 8;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            layoutInflator = (LayoutInflater) ctx.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View view = convertView;
+            view = layoutInflator.inflate(R.layout.nutrition_grid_item_view, parent, false);
+
+            ImageView imageView = (ImageView)view.findViewById(R.id.image);
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+
+            imageView.setImageResource(R.mipmap.ic_launcher);
+
+            return view;
+
+           /* ImageView imageView;
+            if (convertView == null) {
+                imageView = new ImageView(ctx);
+                imageView.setLayoutParams(new GridView.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, 85));
+                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                imageView.setPadding(8, 8, 8, 8);
+            } else {
+                imageView = (ImageView) convertView;
+            }
+
+            imageView.setImageResource(mThumbIds[position]);
+            return imageView;*/
+        }
+    }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
